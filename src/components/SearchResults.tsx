292ba72cd8e31033129d27d42f939fa9e5d394a7
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-01-09 19:15:46 -0300

    Listas virtualizadas

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,6 +1,5 @@
-import { useMemo } from "react"
+import { List, ListRowRenderer, /*AutoSizer*/ } from 'react-virtualized'
 import { ProductItem } from "./ProductItem"
-
 interface SearchResultsProps {
     results: Array<{
         id: number
@@ -13,15 +12,33 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({results, onAddToWishList, totalPrice}: SearchResultsProps) {
+    const rowRenderer: ListRowRenderer = ({ index, key, style}) => {
+        return (
+            <div key={key} style={style}>
+                <ProductItem product={results[index]} onAddToWishList={onAddToWishList} />
+            </div>
+        )
+    }
+
 
     return (
         <div>
             <h2>{totalPrice}</h2>
-            {results.map(product => {
+        <List
+            height={300}
+            rowHeight={30} // altura do item
+            width={900}
+            overscanRowCount={5} //quantos itens são deixados precarregados que não estão visiveis
+            rowCount={results.length} // quantidade de itens
+            rowRenderer={rowRenderer}
+        />
+
+            {/* {results.map(product => {
                 return (
                     <ProductItem key={product.id} product={product} onAddToWishList={onAddToWishList} />
                 )
-            })}
+            })} */}
+
         </div>
     )
 }
